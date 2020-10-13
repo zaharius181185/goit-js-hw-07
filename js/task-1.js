e00cbba25aee955a60ef7f99631e54cfe8873f06
zaharius <zaharius.ltd@gmail.com>
2020-10-13 22:23:37 +0300

    start working

---
+++ js/task-1.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const categoriesRef = document.querySelector('#categories')
+const itemRef = document.querySelectorAll('.item')
+
+console.log(`В списке ${itemRef.length} категории.`)
+
+const countOfItem = itemRef.forEach((element) =>
+console.log(`Категория: ${element.querySelector('h2').textContent} 
+    Количество элементов ${element.querySelectorAll('li').length}`)
+);
+
+
+
+
+
+// Напиши скрипт, который выполнит следующие операции.
+
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
+
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
+
+// Например для первой категории получится:
+
+// Категория: Животные
+// Количество элементов: 4

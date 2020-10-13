79efd19b430fb179771704c1450189a92aa408eb
zaharius <zaharius.ltd@gmail.com>
2020-10-14 01:22:33 +0300

    next work

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,32 @@
 'use strict'
+
+const counterRef = document.querySelector("#counter");
+const buttonIncrementRef = document.querySelector(
+    "button[data-action='increment']"
+  );
+  const buttonDecrementRef = document.querySelector(
+    "button[data-action='decrement']"
+  );
+  const valueRef = document.getElementById("value");
+  
+  const valueIncrement = () => {
+    ++valueRef.textContent;
+  };
+  
+  const valueDecrement = () => {
+    --valueRef.textContent;
+  };
+  
+  buttonIncrementRef.addEventListener("click", valueIncrement);
+  
+  buttonDecrementRef.addEventListener("click", valueDecrement);
+
+
+
+
+
+// Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
+
+// Создай переменную counterValue в которой будет хранится текущее значение счетчика.
+// Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+// Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
--- js/task-5.js
+++ js/task-5.js
@@ -1,1 +1,14 @@
 'use strict'
+
+const inputRef = document.querySelector("#name-input");
+const nameRef = document.querySelector("#name-output");
+
+inputRef.addEventListener("input", (event) => {
+  nameRef.textContent = event.target.value;
+  if (event.target.value.length === 0) {
+    nameRef.textContent = "Незнакомец";
+  }
+});
+
+
+// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка 'незнакомец'.

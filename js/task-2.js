bac1a77ec8494552052cc5642c423744b9d061e6
zaharius <zaharius.ltd@gmail.com>
2020-10-14 00:37:20 +0300

    end work in task-02

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 'use strict'
 
-
+const fragment = document.createElement('ul.ingredients');
 
 const ingredients = [
     'Картошка',
@@ -10,6 +10,16 @@ const ingredients = [
     'Зелень',
     'Приправы',
 ];
+
+const addItemIngredients = ingredients.map((ingredient) => {
+    const ingredientItemRef = document.createElement("li");
+    ingredientItemRef.textContent = ingredient;
+    fragment.appendChild(ingredientItemRef);
+    return fragment;
+    }
+);
+
+console.log(fragment)
 
 
 // Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, после чего вставит все li за одну операцию в список ul.ingredients. Для создания DOM-узлов используй document.createElement().
